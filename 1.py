2c44f45000bc8757b7eb68dbc6461d41fdfeabe7
MantaLawCU <manta.rawiwannawork@gmail.com>
2025-07-14 04:35:45 +0000

    testing age of majority activity

---
+++ 1.py
@@ -0,0 +1,11 @@
+print ("How old are you?)
+input ("age")
+if age < 20:
+    print ("Not of legal age")
+else:
+    married = print ("Are you married?)
+if married = input ("Yes")
+    print ("of legal age")
+else:
+    print("Not of legal age")
+
